19e385d531f1a6383aace86d4045e3b539e81f05
banxi1988 <banxi1988@gmail.com>
2016-12-26 00:31:13 +0800

    first commit

---
+++ mail.js
@@ -0,0 +1,67 @@
+/**
+ * Created by banxi on 12/25/16.
+ */
+
+
+function refDepth(line){
+   var depth = 0;
+    for (var i = 0; i < line.length; i++) {
+        var ch = line[i];
+        if(ch === ' '){
+            continue;
+        }
+        if(ch === '>'){
+            depth++;
+        }else{
+            break;
+        }
+    }
+    return depth;
+}
+
+function createRefNode(lines, depth){
+    var refNode = document.createElement("DIV");
+    var innerRefLines = lines.filter(function (line) {
+        return refDepth(line) === depth + 1;
+    });
+    if (innerRefLines.length > 0){
+        var innerRefNode = createRefNode(lines, depth + 1);
+        refNode.appendChild(innerRefNode);
+    }
+    refNode.className = "ref-node-"+depth;
+    var refLines = lines.filter(function (line) {
+        return refDepth(line) === depth;
+    });
+
+    refNode.innerHTML = refLines.join('<br/>');
+    return refNode;
+}
+
+function createReplyNode(lines) {
+    var replyLines = lines.filter(function (line) {
+        return refDepth(line) === 0;
+    });
+    var replyNode = document.createElement("DIV");
+    replyNode.innerHTML = replyLines.join("<br/>");
+    replyNode.className = "reply";
+    return replyNode;
+}
+
+function main() {
+    var mailBodyPre = document.getElementsByTagName("pre")[0];
+    var rawLines = mailBodyPre.textContent.split('\n');
+    var newContentNode = document.createElement("DIV");
+    var refNode = createRefNode(rawLines, 1);
+    var replyNode = createReplyNode(rawLines);
+
+    newContentNode.appendChild(refNode);
+    newContentNode.appendChild(replyNode);
+
+    var body = mailBodyPre.parentNode;
+    body.appendChild(newContentNode);
+    mailBodyPre.style.display = "none";
+}
+
+main();
+
+
